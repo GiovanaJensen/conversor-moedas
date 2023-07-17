1ecb236305f3cf56dfd903781283c057ec5eb988
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-07-16 22:19:54 -0300

    iniciado o frame

--- br/com/currencyToday/conversorMoedas/frame/Tela1.java
+++ br/com/currencyToday/conversorMoedas/frame/Tela1.java
@@ -1,14 +1,11 @@
-// package br.com.currencyToday.conversorMoedas.frame;
+package br.com.currencyToday.conversorMoedas.frame;
 
-// import javax.swing.JFrame;
-// import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
 
-// public class Tela1 {
-//     public static void main(String[] args){
-//         SwingUtilities.invokeLater(() -> {
-//             JFrame frame = new JFrame();
-//             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//             frame.setVisible(true);
-//         });
-//     }
-// }
+public class Tela1 {
+    public static void main(String[] args){
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+    }
+}
